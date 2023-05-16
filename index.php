4927d30a9955555809e8be96b528a1bd6d9e47b9
Niroj karki <nirojkarki280@gmail.com>
2023-05-16 12:16:13 +0545

    first commitn5

---
+++ index.php
@@ -0,0 +1,318 @@
+<?php 
+    require('top.php');
+?>
+
+
+<div class="body__overlay"></div>
+        
+<!-- Start Slider Area -->
+<div class="slider__container slider--one bg__cat--3">
+    <div class="slide__container slider__activation__wrap owl-carousel">
+
+        <!-- Start Single Slide -->
+        <div class="single__slide animation__style01 slider__fixed--height">
+            <div class="container">
+                <div class="row align-items__center">
+
+                    <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
+                        <div class="slide">
+                            <div class="slider__inner">
+                                <h2>collection 2022</h2>
+                                <h1>Iphone 14 Pro max</h1>
+                                <div class="cr__btn">
+                                    <a href="cart.html">Shop Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
+                        <div class="slide__thumb">
+                            <img src="images/slider/fornt-img/iphone 14 pro max.jpg" alt="slider images">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Single Slide -->
+
+        <!-- Start Single Slide -->
+        <div class="single__slide animation__style01 slider__fixed--height">
+            <div class="container">
+                <div class="row align-items__center">
+                    <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
+                        <div class="slide">
+                            <div class="slider__inner">
+                                <h2>collection 2022</h2>
+                                <h1>Samsung Galaxy S22 Ultra</h1>
+                                <div class="cr__btn">
+                                    <a href="cart.html">Shop Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
+                        <div class="slide__thumb">
+                            <img src="images/slider/fornt-img/samsung S22 ultra.jpg" alt="slider images">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Single Slide -->
+
+         <!-- Start Single Slide -->
+         <div class="single__slide animation__style01 slider__fixed--height">
+            <div class="container">
+                <div class="row align-items__center">
+                    <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
+                        <div class="slide">
+                            <div class="slider__inner">
+                                <h2>collection 2022</h2>
+                                <h1>Realme Narjo 50</h1>
+                                <div class="cr__btn">
+                                    <a href="cart.html">Shop Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
+                        <div class="slide__thumb">
+                            <img src="images/slider/fornt-img/realme narzo 50.jpg" alt="slider images">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Single Slide -->
+
+
+          <!-- Start Single Slide -->
+          <div class="single__slide animation__style01 slider__fixed--height">
+            <div class="container">
+                <div class="row align-items__center">
+                    <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
+                        <div class="slide">
+                            <div class="slider__inner">
+                                <h2>collection 2021</h2>
+                                <h1>Iphone 13 Pro Max</h1>
+                                <div class="cr__btn">
+                                    <a href="cart.html">Shop Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
+                        <div class="slide__thumb">
+                            <img src="images/slider/fornt-img/13.png" alt="slider images">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Single Slide -->
+        
+
+    </div>
+</div>
+<!-- Start Slider Area -->
+
+
+<!-- Start Category Area -->
+<section class="htc__category__area ptb--100">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="section__title--2 text-center">
+                    <h2 class="title__line">New Arrivals</h2>
+                    <p>Here We have some New amazing arrivals that you might enjoy!</p>
+                </div>
+            </div>
+        </div>
+        <div class="htc__product__container">
+            <div class="row">
+                <div class="product__list clearfix mt--30">
+					<?php
+						$get_product= get_product($con,4);
+						foreach($get_product as $list){
+					?>
+                    <!-- Start Single Category -->
+                    <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                        <div class="category">
+                            <div class="ht__cat__thumb">
+                                <a href="product.php?id=<?php echo $list['id']?>">
+                                    <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>" alt="product images">
+                                </a>
+                            </div>
+
+                            <div class="fr__hover__info">
+                                <ul class="product__action">
+                                    <li><a href="javascript:void(0)" onclick="wishlist_manage('<?php echo $list['id'] ?>','add')">
+                                        <i class="icon-heart icons"></i></a>
+                                    </li>
+
+
+                                </ul>
+                            </div>
+                                    
+                            <div class="fr__product__inner">
+                                <h4><a href="product.php">Rs.<?php echo $list['name']?></a></h4>
+                                <ul class="fr__pro__prize">
+                                    <li class="old__prize">Rs.<?php echo $list['mrp']?></li>
+                                    <li><?php echo $list['price']?></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Single Category -->
+					<?php } ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Category Area -->
+
+
+<!-- Start Product Area -->
+<section class="ftr__product__area ptb--100">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="section__title--2 text-center">
+                    <h2 class="title__line">Best Seller</h2>
+                    <p>Here we have our all time best seller phones</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="product__wrap clearfix">
+
+
+                <!-- Start Single Category -->
+                <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                    <div class="category">
+                        <div class="ht__cat__thumb">
+                            <a href="product.php">
+                                <img src="images/product/Iphone/13 Matt black.png" alt="product images">
+                            </a>
+                        </div>
+                        <div class="fr__hover__info">
+                            <ul class="product__action">
+                                    <li><a href="javascript:void(0)" onclick="wishlist_manage('<?php echo $list['id'] ?>','add')">
+                                        <i class="icon-heart icons"></i></a>
+                                    </li>
+
+                                <li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
+
+                                <li><a href="#"><i class="icon-shuffle icons"></i></a></li>
+                            </ul>
+                        </div>
+                        <div class="fr__product__inner">
+                            <h4><a href="product.php">Iphone 13 Pro Max</a></h4>
+                            <ul class="fr__pro__prize">
+                                <li class="old__prize">$659.99</li>
+                                <li>$625</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Single Category -->
+
+
+                <!-- Start Single Category -->
+                <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                    <div class="category">
+                        <div class="ht__cat__thumb">
+                            <a href="product.php">
+                                <img src="images/product/POCO/poco.jpg" style="height: 250px;" alt="product images">
+                            </a>
+                        </div>
+                        <div class="fr__hover__info">
+                            <ul class="product__action">
+                                <li><a href="wishlist.html"><i class="icon-heart icons"></i></a></li>
+
+                                <li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
+
+                                <li><a href="#"><i class="icon-shuffle icons"></i></a></li>
+                            </ul>
+                        </div>
+                        <div class="fr__product__inner">
+                            <h4><a href="product.php">POCO M5</a></h4>
+                            <ul class="fr__pro__prize">
+                                <li class="old__prize">$200</li>
+                                <li>$150.99</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Single Category -->
+
+
+                <!-- Start Single Category -->
+                <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                    <div class="category">
+                        <div class="ht__cat__thumb">
+                            <a href="product.php">
+                                <img src="images/product/xiaomi/xiaomi X12 blue.jpg" alt="product images">
+                            </a>
+                        </div>
+                        <div class="fr__hover__info">
+                            <ul class="product__action">
+                                <li><a href="wishlist.html"><i class="icon-heart icons"></i></a></li>
+
+                                <li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
+
+                                <li><a href="#"><i class="icon-shuffle icons"></i></a></li>
+                            </ul>
+                        </div>
+                        <div class="fr__product__inner">
+                            <h4><a href="product.php">Xiaomi X12</a></h4>
+                                <ul class="fr__pro__prize">
+                                <li class="old__prize">$300.3</li>
+                                <li>$250.9</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Single Category -->
+
+
+                <!-- Start Single Category -->
+                <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                    <div class="category">
+                        <div class="ht__cat__thumb">
+                            <a href="product.php">
+                                <img src="images/product/Iphone/iphone X.jpg" alt="product images">
+                            </a>
+                        </div>
+                        <div class="fr__hover__info">
+                            <ul class="product__action">
+                                <li><a href="wishlist.html"><i class="icon-heart icons"></i></a></li>
+
+                                <li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
+
+                                <li><a href="#"><i class="icon-shuffle icons"></i></a></li>
+                            </ul>
+                        </div>
+                        <div class="fr__product__inner">
+                            <h4><a href="product.php">Iphone X Max</a></h4>
+                            <ul class="fr__pro__prize">
+                                <li class="old__prize">$450.99</li>
+                                <li>$400</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Single Category -->
+
+
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Product Area -->
+
+
+
+<?php require('footer.php')?>        
